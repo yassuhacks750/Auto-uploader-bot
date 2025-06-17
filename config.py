41475e0f2c4776e319cbae684b66d983669388c7
yassuhacks750 <yassudj8085231272@gmail.com>
2025-06-17 22:01:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,12 +3,12 @@ import os
 ##Code Written By @ItsMeMaster
 
 class Config(object):
-    BOT_TOKEN = ""
-    DB_NAME = ""
-    API_ID = 
-    API_HASH = ""
-    ADMIN_ID = 
-    DB_URL = ""
-    LOG_CHANNEL =  # Your Log Channel ID (Bot ko ADMIN BNAYE)
+    BOT_TOKEN = "8171558787:AAFgfebA4U1o1tJujga88XeRS1yk1QJhwRI"
+    DB_NAME = "yassu"
+    API_ID = "22849789"
+    API_HASH = "0fc127c6055acd59f00ec6c229e1e3c4"
+    ADMIN_ID = "7296271316"
+    DB_URL = "mongodb+srv://yassu:Yash2835P@classxbot.bbodynd.mongodb.net/"
+    LOG_CHANNEL = "https://t.me/+aPD-Yb-TUCkzN2U1"
     USERLINK = ""
     TUTORIAL_VIDEO = ""
